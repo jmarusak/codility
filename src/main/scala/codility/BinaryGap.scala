480027ddcf44e4d10d9848d7954406b263dbfcf4
jmarusak <jmarusak@users.noreply.github.com>
2024-05-01 20:21:20 -0400

    bloom support

--- src/main/scala/codility/BinaryGap.scala
+++ src/main/scala/codility/BinaryGap.scala
@@ -1,3 +1,7 @@
 object Solution {
-  def solution(n: Int): Int = 0
-}
+  def solution(n: Int): Int = {
+    val binary = n.toBinaryString
+    val gaps = binary.substring(0, binary.lastIndexOf("1"))
+      .split("1")
+      .map(_.length)
+    if (gaps.isEmpty) 0 else gaps.max
