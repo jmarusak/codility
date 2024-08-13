6949c8306b219adaf60aef17e4714e2e8f67cd0f
jmarusak <jmarusak@users.noreply.github.com>
2024-08-13 15:42:32 -0400

    scala App template

--- python/App.py
+++ python/App.py
@@ -1,5 +1,12 @@
-def main():
-    print('Hello World!')
+import pandas as pd
+
+def merge():
+    df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3']})
+    df2 = pd.DataFrame({'A': ['A0', 'B1', 'B2', 'B3']})
+
+    df = pd.merge(df1, df2, on='A', how='inner') 
+    print(df)
+
 
 if __name__ == '__main__':
-    main()
+    merge() 
